2b723e6bfa3fbc982d92a6c65278e9852b6e3ce2
Jonathan Pastor <jonathancmoa@gmail.com>
2018-05-18 16:51:16 +0200

    [frontend] add a jinja filter for displaying duration

--- frontend.py
+++ frontend.py
@@ -511,6 +511,34 @@ intervals = (
     )
 
 
+@app.template_filter()
+def prettify_duration(dt, default="just now"):
+    """
+    Returns string representing "time since" e.g.
+    3 days ago, 5 hours ago etc.
+    """
+
+    diff = dt
+
+    nb_seconds = diff % 60
+    nb_minutes = (diff - nb_seconds) / 60
+    nb_hours = (diff - nb_seconds - nb_minutes * 60) / 3600
+
+    periods = (
+        (nb_hours, "hour", "hours"),
+        (nb_minutes, "minute", "minutes"),
+        (nb_seconds, "second", "seconds"),
+    )
+
+    result = []
+
+    for period, singular, plural in periods:
+        # if period:
+        result += ["%d %s" % (period, singular if period == 1 else plural)]
+
+    return ", ".join(result)
+
+
 def _display_time(seconds, granularity=2):
     if seconds < 1.0:
         return "now"
