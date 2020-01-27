d52bb1fc7b8222c9ae8b70be5b27fb7e6d5c54f8
Jonathan <jonathan.pastor@me.com>
2020-01-27 17:19:29 +0100

    add reworked modbus test

--- tests/test_modbus_collecters.py
+++ tests/test_modbus_collecters.py
@@ -6,6 +6,7 @@ import requests_mock
 import time
 from tests.utils import mock_data
 
+
 class TestModbusCollecter(unittest.TestCase):
 
     # def setUp(self):
@@ -29,6 +30,9 @@ class TestModbusCollecter(unittest.TestCase):
         cls.modbus_agent = FakeModbusAgent()
         cls.modbus_agent.start()
 
+        cls.modbus_agent_little_endian = FakeModbusAgent(port=5021, endian="little")
+        cls.modbus_agent_little_endian.start()
+
     # def tearDown(self):
     @classmethod
     @mock_data
@@ -47,6 +51,7 @@ class TestModbusCollecter(unittest.TestCase):
 
         # Stop the fake modbus agent
         cls.modbus_agent.stop()
+        cls.modbus_agent_little_endian.stop()
 
     @mock_data
     def test_adding_new_reading(self):
@@ -56,30 +61,46 @@ class TestModbusCollecter(unittest.TestCase):
         from bin.modbus_crawler import new_modbus_reading
         from core.data.influx import db_last_sensors_updates
 
-        with requests_mock.Mocker(real_http=True) as m:
+        expected_values = {
+            "inrow_group_cool_output": 4200,    # big_endian_int32
+            "inrow_group_cool_output2": 1200,   # little_endian_uint32
+            "inrow_group_cool_output3": 6400,   # big_endian_int16
+            "inrow_group_cool_output4": 12800,  # little_endian_uint16
+            "inrow_group_cool_output5": 25600,  # big_endian_float32
+            "inrow_group_cool_output_little": 4200,  # big_endian_int32
+            "inrow_group_cool_output2_little": 1200,  # little_endian_uint32
+            "inrow_group_cool_output3_little": 6400,  # big_endian_int16
+            "inrow_group_cool_output4_little": 12800,  # little_endian_uint16
+            "inrow_group_cool_output5_little": 25600,  # big_endian_float32
+        }
 
-            parameters = get_influxdb_parameters()
-            db_client = get_influxdb_client()
+        # with requests_mock.Mocker(real_http=True) as m:
 
-            modbus_sensors = get_modbus_sensors()
+        parameters = get_influxdb_parameters()
+        db_client = get_influxdb_client()
 
-            for modbus_sensor in modbus_sensors:
-                new_modbus_reading(modbus_sensor)
+        modbus_sensors = get_modbus_sensors()
 
-            last_sensor_updates = db_last_sensors_updates()
+        for modbus_sensor in modbus_sensors:
+            new_modbus_reading(modbus_sensor)
 
-            self.assertEqual(len(last_sensor_updates), 1)
+        last_sensor_updates = db_last_sensors_updates()
 
-            for sensor_reading in last_sensor_updates:
-                self.assertIn(sensor_reading.get("sensor"), ['inrow_group_cool_output'])
-                self.assertGreaterEqual(sensor_reading.get("last_value"), 4200)
-                self.assertLessEqual(sensor_reading.get("last_value"), 4200)
-                self.assertIn(sensor_reading.get("location"), ["not\\ specified"])
-                self.assertIn(sensor_reading.get("unit"), ['W'])
-                self.assertIn(sensor_reading.get("sensor_type"), ['wattmeter'])
+        self.assertEqual(len(last_sensor_updates), len(list(expected_values.keys())))
 
-                reading_date = arrow.get(sensor_reading.get("time"))
-                self.assertLessEqual(abs(reading_date.timestamp - time.time()), 120)
+        for sensor_reading in last_sensor_updates:
+
+            expected_value = expected_values.get(sensor_reading.get("sensor"))
+
+            self.assertIn(sensor_reading.get("sensor"), list(expected_values.keys()))
+            self.assertGreaterEqual(sensor_reading.get("last_value"), expected_value)
+            self.assertLessEqual(sensor_reading.get("last_value"), expected_value)
+            self.assertIn(sensor_reading.get("location"), ["not\\ specified"])
+            self.assertIn(sensor_reading.get("unit"), ['W'])
+            self.assertIn(sensor_reading.get("sensor_type"), ['wattmeter'])
+
+            reading_date = arrow.get(sensor_reading.get("time"))
+            self.assertLessEqual(abs(reading_date.timestamp - time.time()), 120)
 
 
 if __name__ == '__main__':
