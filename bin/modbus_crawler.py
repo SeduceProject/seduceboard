acea39e7f6066745ee2287632781d48b5ccbeea2
Jonathan <jonathan.pastor@me.com>
2020-01-27 18:53:21 +0100

    save work

--- bin/modbus_crawler.py
+++ bin/modbus_crawler.py
@@ -1,17 +1,23 @@
 from __future__ import absolute_import, unicode_literals
 
-import threading
 import time
 import traceback
-from pyModbusTCP.client import ModbusClient
+from docopt import docopt
 from pymodbus.client.sync import ModbusTcpClient
-
-from core.data.influx import get_influxdb_client
-from core.collecters.utils import set_interval
 from pymodbus.payload import BinaryPayloadDecoder
+from core.collecters.utils import set_interval
+from core.data.influx import get_influxdb_client
+import threading
 
 DEBUG = True
 LAST_TIMESTAMP_INSERTED = {}
+FLUSH_FREQUENCY_SECONDS = 1.0
+ACQUISITION_WINDOW_DURATION = 1.0
+
+RECORDS = []
+CONNECTION_POOL = {}
+
+influx_lock = threading.Lock()
 
 
 def modbus_read_int(client, address, nb_char):
@@ -28,7 +34,9 @@ def modbus_read_int(client, address, nb_char):
 
 
 def new_modbus_reading(config):
-    db_client = get_influxdb_client()
+    global influx_lock
+    global RECORDS
+    global CONNECTION_POOL
 
     modbus_ip = config.get("ip")
     modbus_port = config.get("port", 502)
@@ -41,6 +49,8 @@ def new_modbus_reading(config):
     sensor_location = config.get("location", "not specified")
     divide_factor = config.get("modbus_divide_by", 1)
     multiply_factor = config.get("modbus_multiply_by", 1)
+
+    print(f">> {sensor_name}")
 
     insertion_count = 0
 
@@ -60,11 +70,15 @@ def new_modbus_reading(config):
     else:
         raise Exception(f"Could not understand how to parse the following data_type '{data_type}'")
 
-    client = ModbusTcpClient(modbus_ip, port=modbus_port)
-    client.connect()
+    connection_key = f"{modbus_ip}_{modbus_port}"
+    if connection_key not in CONNECTION_POOL:
+        CONNECTION_POOL[connection_key] = ModbusTcpClient(modbus_ip, port=modbus_port)
+        CONNECTION_POOL[connection_key].connect()
+
+    client = CONNECTION_POOL[connection_key]
 
     value = client.read_holding_registers(modbus_register, read_count_bytes, unit=1)
-    client.close()
+    # client.close()
 
     # Default byte order
     default_byteorder = '<'
@@ -72,6 +86,9 @@ def new_modbus_reading(config):
 
     byteorder = config.get("byteorder", default_byteorder)
     wordorder = config.get("wordorder", default_wordorder)
+
+    if not hasattr(value, "registers"):
+        print(f"ERROR with {sensor_name}")
 
     decoder = BinaryPayloadDecoder.fromRegisters(value.registers, byteorder=byteorder, wordorder=wordorder)
 
@@ -129,33 +146,64 @@ def new_modbus_reading(config):
         }
     }]
 
+    # Put the modbus sensor's value in the list of values that should be inserted in DB
+    influx_lock.acquire()
+    RECORDS += data
+    influx_lock.release()
+
+    return False
+
+
+def flush_records(args):
+    global influx_lock
+    global RECORDS
+    db_client = get_influxdb_client()
+
+    influx_lock.acquire()
+    flush_data = RECORDS
+    RECORDS = []
+    influx_lock.release()
+
     try:
-        failure = not db_client.write_points(data, time_precision="s")
+        db_client.write_points(flush_data, time_precision="s")
+        print("[influx] %s rows have been inserted in the database" % (len(flush_data)))
     except :
         traceback.print_exc()
-        failure = True
 
     db_client.close()
 
-    if failure:
-        print("[%s] failed to insert rows in the database" % (sensor_name))
-        return {"status": "failure", "reason": "could not write in the DB"}
-    else:
-        insertion_count += 1
-
-    print("[%s] %s rows have been inserted in the database" % (sensor_name, insertion_count))
-    return {"status": "success", "update_count": insertion_count}
+    return False
 
 
 if __name__ == "__main__":
     from core.config.crawlers_config import get_modbus_sensors
 
+    help = """MODBUS crawler
+
+Usage:
+  modbus_crawler.py [--sensor_bus=<sensor_bus>]
+
+Options:
+  -h --help          Show this message and exit.
+"""
+    arguments = docopt(help)
+
+    selected_sensors = get_modbus_sensors()
+
+    if arguments.get("--sensor_bus", "*") not in ["*", None]:
+        parent_pattern = arguments["--sensor_bus"]
+        selected_sensors = [sensor for sensor in selected_sensors if parent_pattern in sensor.get("parent", '')]
+
+    selected_sensors = selected_sensors
+
     modbus_readers = []
-    for config in get_modbus_sensors():
+    for config in selected_sensors:
         modbus_readers += [set_interval(new_modbus_reading, (config, ), 1)]
 
         # To prevent all sensors to crawl in parallel
-        time.sleep(2)
+        time.sleep(ACQUISITION_WINDOW_DURATION / max(len(selected_sensors), 1))
+
+    flusher = set_interval(flush_records, (selected_sensors,), FLUSH_FREQUENCY_SECONDS)
 
     try:
         for modbus_reader in modbus_readers:
