2be07b11fc4f898930754ac1f9857458b18cd297
RP <remy.pottier@imt-atlantique.fr>
2021-03-30 08:54:10 +0200

    DB_NAME as a variable

--- conf/grafana/influx-CQ.sh
+++ conf/grafana/influx-CQ.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
+DB_NAME="mondb"
 
-influx -database pidiou -execute "CREATE CONTINUOUS QUERY aggregated_servers_Z1_1 ON pidiou BEGIN \
+influx -database $DB_NAME -execute "CREATE CONTINUOUS QUERY aggregated_servers_Z1_1 ON $DB_NAME BEGIN \
   SELECT SUM(value) INTO aggregated_Z1_1 FROM sensors \
   WHERE sensor =~ /ecotype.*Z1.1/ \
   GROUP BY location, time(1s) \
 END"
-influx -database pidiou -execute "CREATE CONTINUOUS QUERY aggregated_servers_Z1_2 ON pidiou BEGIN \
+influx -database $DB_NAME -execute "CREATE CONTINUOUS QUERY aggregated_servers_Z1_2 ON $DB_NAME BEGIN \
   SELECT SUM(value) INTO aggregated_Z1_2 FROM sensors \
   WHERE sensor =~ /ecotype.*Z1.2/ \
   GROUP BY location, time(1s) \
 END"
-influx -database pidiou -execute "CREATE CONTINUOUS QUERY aggregated_servers_Z1_4 ON pidiou BEGIN \
+influx -database $DB_NAME -execute "CREATE CONTINUOUS QUERY aggregated_servers_Z1_4 ON $DB_NAME BEGIN \
   SELECT SUM(value) INTO aggregated_Z1_4 FROM sensors \
   WHERE sensor =~ /ecotype.*Z1.4/ \
   GROUP BY location, time(1s) \
 END"
-influx -database pidiou -execute "CREATE CONTINUOUS QUERY aggregated_servers_Z1_5 ON pidiou BEGIN \
+influx -database $DB_NAME -execute "CREATE CONTINUOUS QUERY aggregated_servers_Z1_5 ON $DB_NAME BEGIN \
   SELECT SUM(value) INTO aggregated_Z1_5 FROM sensors \
   WHERE sensor =~ /ecotype.*Z1.5/ \
   GROUP BY location, time(1s) \
